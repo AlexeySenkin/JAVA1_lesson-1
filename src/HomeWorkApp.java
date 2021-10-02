f3ac4cb72bd52a387649b3580b42cbba1c2e67c4
AlexeySenkin <Senkinalexey@mail.ru>
2021-10-02 23:52:22 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,50 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        System.out.println("---------");
+        printThreeWords();
+        System.out.println("---------");
+        checkSumSign();
+        System.out.println("---------");
+        printColor();
+        System.out.println("---------");
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = -9;
+        int b = 9;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(){
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100 ) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = -9;
+        int b = -9;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+}
